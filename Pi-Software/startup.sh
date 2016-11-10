d1c6b623bd3c3caf5c02ec90087ea344287ad9e6
Mike Koch <mkoch227@gmail.com>
2016-11-10 00:20:22 +0000

    Startup script now properly exits bash when Ctrl+C'ed

--- Pi-Software/startup.sh
+++ Pi-Software/startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Trap function so we can exit the bash shell if the user hits Ctrl+C
+trap ctrl_c INT
+
+function ctrl_c() {
+	clear
+	printf "Abort detected!\n"
+	exit
+}
+
 # Launch a splash screen giving the user 10 seconds to cancel the startup sequence
 clear
 printf "  ______ _                _____  \n"
@@ -9,7 +19,7 @@ printf " | |    | | |_| |/ ____ \| | \ \ \n"
 printf " |_|    |_|\__, /_/    \_\_|  \_\\n"
 printf "            __/ |                \n"
 printf "           |___/                 \n"
-printf "FlyAR Starting Up. Press Ctrl-C within 10 seconds to abort."
+printf "FlyAR Starting Up. Press Ctrl-C within 10 seconds to abort.\n"
 
 sleep 10
 
