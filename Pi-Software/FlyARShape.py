3c625dc871f771895ac2a3bf2e0bd0681e303277
Mike Koch <mkoch227@gmail.com>
2016-11-14 18:44:59 -0500

    Refactoring; pull shape class to separate py file

---
+++ Pi-Software/FlyARShape.py
@@ -0,0 +1,39 @@
+# An actual shape object to be used by the 3D rendering script. Contains all necessary information
+class FlyARShape():
+    def __init__(self):
+        self._shapeType = None
+        self._color = None
+        self._position = None
+        self._radii = None
+
+    @property
+    def shapeType(self):
+        return self._shapeType
+    
+    @shapeType.setter
+    def shapeType(self, value):
+        self._shapeType = value
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        self._color = value
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        self._position = value
+
+    @property
+    def radii(self):
+        return self._xRadius
+
+    @radii.setter
+    def radii(self, value):
+        self._radii = value
