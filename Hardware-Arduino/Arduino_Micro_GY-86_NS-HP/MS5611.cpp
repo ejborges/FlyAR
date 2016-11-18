58aa8dc227213bbf9d3cdccff44a1f19d8ddf355
ejborges <emilio.j.borges@gmail.com>
2016-11-18 04:11:47 -0500

    working on MS5611 I2C commands; done with temp & pressure calculations

--- Hardware-Arduino/Arduino_Micro_GY-86_NS-HP/MS5611.cpp
+++ Hardware-Arduino/Arduino_Micro_GY-86_NS-HP/MS5611.cpp
@@ -76,3 +76,149 @@ void MS5611::testConnection() {
 //    }
     return false;
 }
+
+/** Calculate the difference between actual and reference temperature
+ * @return signed integral temperature difference value between -16776960 and 16777216
+ */
+int32_t getTempDifference_dT(){
+    // dT = D2 - T_REF = D2 - C5*(2^8)
+    return D2 - ((uint32_t)C5 << 8); // = D2 - T_REF = D2 - C5*(2^8)
+}
+
+/** Calculate temperature in celsius from given PROM calibration and digital temperature data.
+ * @return signed integral temperature value between -4000 <= TEMP <= 8500; ex. 2007 = 20.07 C
+ */
+int32_t MS5611::getTemperature_int() {
+    #ifndef SECOND_ORDER_TEMP_COMPENSATION
+    //   dT = D2 - T_REF = D2 - C5*(2^8)
+    // TEMP = (20 C) + dT * TEMPSENS = 2000 + (dT * C6)/(2^23)
+    //      = 2000 + ((D2 - C5*(2^8)) * C6)/(2^23)
+    return 2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23);
+    #else
+    int32_t TEMP = 2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23);
+    float T2 = 0.0;
+    if(TEMP < 2000) T2 = ((D2 - ((uint32_t)C5 << 8))*(D2 - ((uint32_t)C5 << 8))) / ((uint32_t)1 << 31);
+    return (int32_t)((float)TEMP - T2);
+    #endif
+}
+
+/** Calculate temperature in celsius from given PROM calibration and digital temperature data.
+ * @return signed float temperature value between -40.00 <= TEMP <= 85.00 C
+ */
+float MS5611::getTemperature_float() {
+    #ifndef SECOND_ORDER_TEMP_COMPENSATION
+    //   dT = D2 - T_REF = D2 - C5*(2^8)
+    // TEMP = (20 C) + dT * TEMPSENS = 2000 + (dT * C6)/(2^23)
+    //      = 2000 + ((D2 - C5*(2^8)) * C6)/(2^23)
+    return (2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23)) / 100.0;
+    #else
+    float TEMP = (2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23)) / 100.0;
+    float T2 = 0.0;
+    if(TEMP < 20.00) T2 = ((D2 - ((uint32_t)C5 << 8))*(D2 - ((uint32_t)C5 << 8))) / ((uint32_t)1 << 31);
+    return TEMP - T2;
+    #endif
+
+    // https://github.com/AeroQuad/AeroQuad/blob/master/Libraries/AQ_BarometricSensor/BarometricSensor_MS5611.h
+    // return ((1<<5)*2000 + (((D2 - ((int64_t)C5 << 8)) * C6) >> (23-5))) / ((1<<5) * 100.0);
+}
+
+/** Calculate offset at actual temperature from given PROM calibration and digital temperature data.
+ * @return signed 64-bit integral value between -8589672450 and 12884705280
+ */
+int64_t getTempOffset(){
+    #ifndef SECOND_ORDER_TEMP_COMPENSATION
+    // OFF = OFF_T1 + TCO * dT
+    //     = C2*(2^16) + (C4 * dT)/(2^7)
+    //     = C2*(2^16) + (C4 * (D2 - C5*(2^8)))/(2^7)
+    return (((uint64_t)C2 << 16) + ((C4 * (D2 - ((uint64_t)C5 << 8))) >> 7));
+    #else
+    int64_t TEMP = 2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23);
+    int64_t OFF = (((uint64_t)C2 << 16) + ((C4 * (D2 - ((uint64_t)C5 << 8))) >> 7));
+    int64_t OFF2 = 0;
+    if(TEMP < 2000) {
+        OFF2 = (5 * ((TEMP - 2000)*(TEMP - 2000))) >> 1;
+        if(TEMP < -1500) OFF2 += (7*((TEMP + 1500)*(TEMP + 1500)));
+    }
+    return OFF - OFF2;
+    #endif
+}
+
+/** Calculate sensitivity at actual temperature from given PROM calibration and digital temperature data.
+ * @return signed 64-bit integral value between -4294836225 and 6442352640
+ */
+int64_t getTempSensitivity(){
+    #ifndef SECOND_ORDER_TEMP_COMPENSATION
+    // SENS = SENS_T1 + TCS * dT
+    //      = C1*(2^15) + (C3 * dT)/(2^8)
+    //      = C1*(2^15) + (C3 * (D2 - C5*(2^8)))/(2^8)
+    return (((uint64_t)C1 << 15) + ((C3 * (D2 - ((uint64_t)C5 << 8))) >> 8));
+    #else
+    int64_t TEMP = 2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23);
+    int64_t SENS = (((uint64_t)C1 << 15) + ((C3 * (D2 - ((uint64_t)C5 << 8))) >> 8));
+    int64_t SENS2 = 0;
+    if(TEMP < 2000) {
+        SENS2 = (5 * ((TEMP - 2000)*(TEMP - 2000))) >> 2;
+        if(TEMP < -1500) SENS2 += ((11*((TEMP + 1500)*(TEMP + 1500))) >> 1);
+    }
+    return SENS - SENS2;
+    #endif
+}
+
+/** Calculate temperature compensated pressure from given PROM calibration, digital pressure, and temperature data.
+ * @return signed integral pressure value between 1000 <= P <= 120000; ex. 100009 = 1000.09 mbar
+ */
+int32_t MS5611::getPressure_int() {
+    #ifndef SECOND_ORDER_TEMP_COMPENSATION
+    // P = D1 * SENS - OFF
+    //   = (((D1 * SENS) / (2^21)) - OFF) / (2^15)
+    int64_t OFF = (((uint64_t)C2 << 16) + ((C4 * (D2 - ((uint64_t)C5 << 8))) >> 7));
+    int64_t SENS = (((uint64_t)C1 << 15) + ((C3 * (D2 - ((uint64_t)C5 << 8))) >> 8));
+    #else
+    int64_t TEMP = 2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23);
+    int64_t OFF = (((uint64_t)C2 << 16) + ((C4 * (D2 - ((uint64_t)C5 << 8))) >> 7));
+    int64_t OFF2 = 0;
+    int64_t SENS = (((uint64_t)C1 << 15) + ((C3 * (D2 - ((uint64_t)C5 << 8))) >> 8));
+    int64_t SENS2 = 0;
+    if(TEMP < 2000) {
+        OFF2 = (5 * ((TEMP - 2000)*(TEMP - 2000))) >> 1;
+        SENS2 = (5 * ((TEMP - 2000)*(TEMP - 2000))) >> 2;
+        if(TEMP < -1500) {
+            OFF2 += (7*((TEMP + 1500)*(TEMP + 1500)));
+            SENS2 += ((11*((TEMP + 1500)*(TEMP + 1500))) >> 1);
+        }
+    }
+    OFF -= OFF2;
+    SENS -= SENS2;
+    #endif
+    return (((D1 * SENS) >> 21) - OFF) >> 15;
+}
+
+/** Calculate temperature compensated pressure given PROM calibration, digital pressure, and temperature data.
+ * @return signed float pressure value between 10.00 <= P <= 1200.00 mbar
+ */
+float MS5611::getPressure_float() {
+    #ifndef SECOND_ORDER_TEMP_COMPENSATION
+    // P = D1 * SENS - OFF
+    //   = (((D1 * SENS) / (2^21)) - OFF) / (2^15)
+    int64_t OFF = (((uint64_t)C2 << 16) + ((C4 * (D2 - ((uint64_t)C5 << 8))) >> 7));
+    int64_t SENS = (((uint64_t)C1 << 15) + ((C3 * (D2 - ((uint64_t)C5 << 8))) >> 8));
+    #else
+    int64_t TEMP = 2000 + (((D2 - ((uint64_t)C5 << 8))*C6) >> 23);
+    int64_t OFF = (((uint64_t)C2 << 16) + ((C4 * (D2 - ((uint64_t)C5 << 8))) >> 7));
+    int64_t OFF2 = 0;
+    int64_t SENS = (((uint64_t)C1 << 15) + ((C3 * (D2 - ((uint64_t)C5 << 8))) >> 8));
+    int64_t SENS2 = 0;
+    if(TEMP < 2000) {
+        OFF2 = (5 * ((TEMP - 2000)*(TEMP - 2000))) >> 1;
+        SENS2 = (5 * ((TEMP - 2000)*(TEMP - 2000))) >> 2;
+        if(TEMP < -1500) {
+            OFF2 += (7*((TEMP + 1500)*(TEMP + 1500)));
+            SENS2 += ((11*((TEMP + 1500)*(TEMP + 1500))) >> 1);
+        }
+    }
+    OFF -= OFF2;
+    SENS -= SENS2;
+    #endif
+    return ((((D1 * SENS) >> 21) - OFF) >> 15) / 100.0;
+}
+
--- Hardware-Arduino/Arduino_Micro_GY-86_NS-HP/MS5611.h
+++ Hardware-Arduino/Arduino_Micro_GY-86_NS-HP/MS5611.h
@@ -41,11 +41,21 @@ THE SOFTWARE.
 
 #include "I2Cdev.h"
 
-#define MS5611_ADDRESS_CSB_LOW      0x77 // CSB pin low (GND), default for default for InvenSense evaluation board and GY-86
+#define MS5611_ADDRESS_CSB_LOW      0x77 // CSB pin low (GND), default for InvenSense evaluation board and GY-86 board
 #define MS5611_ADDRESS_CSB_HIGH     0x76 // CSB pin high (VCC)
 #define MS5611_DEFAULT_ADDRESS      MS5611_ADDRESS_CSB_LOW
 
-#define xxx 0
+// better temperature and pressure accuracy, may run a bit slower, highly recommended if temp below 20 C
+//#define SECOND_ORDER_TEMP_COMPENSATION
+
+//#define DEFINE_CONSTANTS
+#ifdef DEFINE_CONSTANTS
+    #define P_MIN_MBAR 1000
+    #define P_MAX_MBAR 120000
+    #define T_MIN_C (-4000)
+    #define T_MAX_C 8500
+    #define T_REF_C 2000
+#endif
 
 class MS5611 {
 public:
@@ -55,8 +65,56 @@ public:
     void initialize();
     bool testConnection();
 
+    // I2C commands
+    void reset();
+    void readPROM();
+    void D1Conversion();
+    void D2Conversion();
+    void readADCResult();
+
+    // calibration data from PROM
+    uint16_t C1;    // Pressure sensitivity | SENS_T1
+    uint16_t C2;    // Pressure offset | OFF_T1
+    uint16_t C3;    // Temperature coefficient of pressure sensitivity | TCS
+    uint16_t C4;    // Temperature coefficient of pressure offset | TCO
+    uint16_t C5;    // Reference temperature | T_REF
+    uint16_t C6;    // Temperature coefficient of the temperature | TEMPSENS
+
+    // digital pressure and temperature data
+    uint32_t D1;    // Digital pressure value
+    uint32_t D2;    // Digital temperature value
+
+    // temperature calculation variables
+    //int32_t dT;         // Difference between actual and reference temperature
+    //int32_t TEMP;       // Actual temperature (-40...85C with 0.01C resolution)
+    //float TEMP_float;   // -4000 <= TEMP <= 8500; ex. 2007 = 20.07 C
+
+    // pressure calculation variables
+    //int64_t OFF;    // Offset at actual temperature
+    //int64_t SENS;   // Sensitivity at actual temperature
+    //int32_t P;      // Temperature compensated pressure (10...1200 mbar with 0.01 mbar resolution)
+    //float P_float;  // 1000 <= P <= 120000; ex. 100009 = 1000.09 mbar
+
+    // calculation functions
+    int32_t getTempDifference_dT(); // Difference between actual and reference temperature; -16776960 <= dT <= 16777216
+    int32_t getTemperature_int();   // Actual temperature (-40...85C with 0.01C resolution) -4000 <= TEMP <= 8500;
+    float getTemperature_float();   // Actual temperature (-40...85C with 0.01C resolution) -40.00 <= TEMP <= 85.00;
+    int64_t getTempOffset();        // Offset at actual temperature; -8589672450 <= OFF <= 12884705280
+    int64_t getTempSensitivity();   // Sensitivity at actual temperature; -4294836225 <= OFF <= 6442352640
+    int32_t getPressure_int();      // Temperature compensated pressure (1000 to 120000)
+    float getPressure_float();      // Temperature compensated pressure (10.00 to 1200.00 mbar with 0.01 resolution)
+
+
 private:
     uint8_t devAddr;
+
+    #ifndef DEFINE_CONSTANTS
+    const int32_t P_MIN_MBAR = 1000;
+    const int32_t P_MAX_MBAR = 120000;
+    const int32_t T_MIN_C = -4000;
+    const int32_t T_MAX_C = 8500;
+    const int32_t T_REF_C = 2000;
+    #endif
 };
 
 #endif //_MS5611_H_
